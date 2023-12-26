703b89b4b20f4dba8fa078cb7a99219a4ab720f0
Lucas Araujo <lucasao744@gmail.com>
2023-12-26 17:33:30 -0300

    Aprendendo variavel de estado: useState()

--- src/components/Topo.js
+++ src/components/Topo.js
@@ -1,35 +1,49 @@
-
+import { useState } from "react"
 function Topo() {
-    let nomeDoUsuario = "Lucas";//prompt("Qual é seu nome?");
-    let novaFoto = "https://vmxi.com.br/img/login.png";//prompt("Coloque o Link Da Sua Foto de perfil:");
-    let fotoInicial = "https://vmxi.com.br/img/login.png"; 
-    
-    let frase 
+    //variavel de estado 
+    const [nomeDoUsuario, setNomeDoUsuario] = useState("");
+    const [foto, setfoto] = useState("https://vmxi.com.br/img/login.png");
+    const [cor, setCor] = useState("verde");
+
+    let frase
     //if(!nomeDoUsuario){ pode se usar assim para fazer a ferificação(nao é um boa pratica)
-     if(nomeDoUsuario === "" || nomeDoUsuario === undefined || nomeDoUsuario === null){
-      frase = "Olá, Não sabemos o seu nome!";
-     }else{
-     frase = `seja bem vindo(a), ${nomeDoUsuario}!`;
-     }
+    if (nomeDoUsuario === "" || nomeDoUsuario === undefined || nomeDoUsuario === null) {
+        frase = "Olá, Não sabemos o seu nome!";
+    } else {
+        frase = `seja bem vindo(a), ${nomeDoUsuario}!`;
+    }
     //Tmb pode se usar Ternario: (condicão) ? (se for true) : (se for false); serve para fazer modificações simples ou verificações true ou false "somente"
 
-     function apertabotao(){
-        alert("vou pedir o seu nome!");
+    function apertabotao() {
+        const novoNome = prompt("Qual é seu nome?");
+        setNomeDoUsuario(novoNome)
+    }
 
-     }
+    function trocaFoto() {
+        const novaFoto = prompt("Coloque o Link Da Sua Foto de perfil:");
+        setfoto(novaFoto);
+    }
 
+    function trocaCor(){
+        if (cor === "branco"){
+            setCor("azul")
+        }else{
+            setCor("branco")
+        }
+    }
     //se a função não tiver parâmetros => é só colocar o nome dela 
     //se a função tiver parâmetros => colocar arrow function ante
-        //EX: () => somar(6, 7)
+    //EX: () => somar(6, 7)
 
     return (
 
         <div class="topo">
             <h1>CineFlix</h1>
-            <div>
+            <div>       
+            <button  onClick={trocaCor} class={cor}>Trocar Cor</button>
                 <button onClick={apertabotao}>Inserir  nome</button>
                 <p>{frase}</p>
-                <img src={novaFoto ? novaFoto : fotoInicial} />
+                <img onClick={trocaFoto} src={foto} />
             </div>
         </div>
     );
