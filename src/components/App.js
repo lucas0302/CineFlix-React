91af3a87e36eff8869ab357a436e03c7424681aa
Lucas Araujo <lucasao744@gmail.com>
2023-12-27 19:39:25 -0300

    fragment

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,14 @@ import Menu from "./Menu"
 import Conteudo from "./Conteudo"
 export default function App() {
     return (
-        <div>
-
+        <>
             <Topo />
-
             <div className="row">
                 <Menu />
                 <Conteudo />
                 {/*Pode ser assim <Conteudo></Conteudo> ou assim <Conteudo/> as tegs */}
             </div>
-        </div>
+        </>
 
     );
 }
